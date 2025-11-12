8d0c9c0a6dbcf2c2807f699687026e4940520560
Pete H <pete@kawumtech.de>
2025-11-12 01:22:25 +0100

    remove default app code

--- app.go
+++ app.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
 )
 
 // App struct
@@ -19,9 +20,6 @@ func NewApp() *App {
 // so we can call the runtime methods
 func (a *App) startup(ctx context.Context) {
 	a.ctx = ctx
-}
-
-// Greet returns a greeting for the given name
-func (a *App) Greet(name string) string {
-	return fmt.Sprintf("Hello %s, It's show time!", name)
+	// TEST user home
+	fmt.Println("USER HOME: ", os.Getenv("HOME"))
 }
